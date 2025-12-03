fb1abbdacc2f501c4008ae88a8d57e1f2036cb4a
I4NC0STA <iancosta2026@gmail.com>
2025-12-03 10:42:08 -0300

    ultimos ajustes

--- projeto/menu.php
+++ projeto/menu.php
@@ -49,6 +49,8 @@ if (!isset($_SESSION)) session_start();
         <li class="nav-item"><a class="nav-link active" href="carros.php">Ofertas</a></li>
         <li class="nav-item"><a class="nav-link" href="quemsomos.php">Quem somos</a></li>
         <li class="nav-item"><a class="nav-link" href="faleconosco.php">Fale Conosco</a></li>
+        <li class="nav-item"><a class="nav-link" href="adm.php">ADM</a></li>
+        
       </ul>
     </div>
 
